b68d0479a7cf0bdb16648c8a2384ad015517e24a
Emilie G <adamanska@gmail.com>
2018-07-11 20:26:39 +0200

    adding notifications and tabs awareness

--- accountable/src/App.js
+++ accountable/src/App.js
@@ -9,8 +9,55 @@ import {
   Question, Input, Button
 } from './components';
 
+
+let hidden, visibilityChange;
+let activeTab = true;
+if (typeof document.hidden !== "undefined") {
+  hidden = "hidden";
+  visibilityChange = "visibilitychange";
+}
+else if (typeof document.mozHidden !== "undefined") {
+  hidden = "mozHidden";
+  visibilityChange = "mozvisibilitychange";
+}
+else if (typeof document.msHidden !== "undefined") {
+  hidden = "msHidden";
+  visibilityChange = "msvisibilitychange";
+}
+else if (typeof document.webkitHidden !== "undefined") {
+  hidden = "webkitHidden";
+  visibilityChange = "webkitvisibilitychange";
+}
+
+function handleVisibilityChange() {
+  if (document[hidden]) {
+    //Not visible, Do whatever
+    console.log("HIDDEN")
+    //changeActiveTab()
+    activeTab=false;
+    console.log(activeTab)
+  }
+  else {
+    //Visible
+    console.log("VISIBLE")
+    // changeActiveTab()
+    activeTab=true;
+    console.log(activeTab)
+  }
+}
+
+if (typeof document.addEventListener === "undefined" ||
+  typeof document[hidden] === "undefined") {
+  //alert("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
+}
+else {
+  document.addEventListener(visibilityChange, handleVisibilityChange, false);
+}
+
+
 class App extends Component {
   // not2 = () => toast("7 Kingdoms", { autoClose: 7000 });
+
   constructor() {
     super();
     this.showNotifications = this.showNotifications.bind(this);
@@ -19,24 +66,56 @@ class App extends Component {
     this.updateDisplayedTime = this.updateDisplayedTime.bind(this);
     this.takeOutOneSec = this.takeOutOneSec.bind(this);
     this.getTimeRemaining = this.getTimeRemaining.bind(this);
+    this.textChoice = this.textChoice.bind(this);
+    this.gotNewNotification = this.gotNewNotification.bind(this);
 
     this.state = {
       seconds: 0,
+      initialSeconds: 0,
       hours: "",
       minutes: "",
       activity: "",
       remainingMinutes: 0,
       remainingSeconds: 0,
-      intro: []
+      start: false,
+      middle: false,
+      closing: false,
+      done: false,
+      intro: [],
+      dialog: [],
+      wellDone: [],
+      almostTimeUp: [],
+      selected: "Welcome.",
     };
   }
 
-  componentDidMount () {
+  componentDidMount() {
     // Simulate API response
-    const intro = [
+    const intro = [ //smile
       "Are you ready to give it your all?",
+      "Glad to see you're ready to work.",
     ]
     this.setState({ intro })
+
+    const dialog = [ //angry
+      "You're giving it 100%? Give it 120%!",
+      "Focus on a single task and let nothing stop you!",
+    ]
+    this.setState({ dialog })
+
+    const wellDone = [ //smile
+      "Alright! We're done!",
+      "I hope you're proud of yourself.",
+    ]
+    this.setState({ wellDone })
+
+    const almostTimeUp = [ //angry
+      "Time is almost up. Last push!",
+      "Almost done? Keep going!",
+    ]
+    this.setState({ almostTimeUp })
+
+
   }
 
   showNotifications() {
@@ -50,11 +129,9 @@ class App extends Component {
 
   // =============== P O P U P ==================
 
-  gotNewNotification(title, text, img) { // empty is target webaddress
-    Notifier.start("Hey you!", "Get your shit together!", "", "https://www.fightersgeneration.com/np7/char/hayato-rs-bust.png");
+  gotNewNotification(text, img) { // empty is target webaddress
+    Notifier.start("Hey you!", text, "", img);
   }
-
-
 
   // =============== C O U N T D OW N ==================
 
@@ -69,18 +146,30 @@ class App extends Component {
     this.setState({ seconds: seconds }, () => {
       console.log("TOTAL", this.state.seconds)
     });
+    this.setState({ initialSeconds: seconds, start: true });
+    this.textChoice("intro");
+    console.log(activeTab)
     // After one second take out one second
     setTimeout(this.takeOutOneSec, 1000)
+    
   }
 
   takeOutOneSec() {
-    if (this.state.seconds === 0) {
-      console.log("TIME UP")
-      this.setState({ minutes: "" })
-      this.setState({ hours: "" })
+    if (this.state.seconds === 0) { //  T I M E   I S   U P
+      this.setState({ minutes: "", hours: "", closing: false, middle: false, start: false, done: true })
+      this.textChoice("wellDone");
       return
     } else {
       this.setState({ seconds: this.state.seconds - 1 })
+      // checking where we are in the time
+      if (this.state.seconds === this.state.initialSeconds - 900 && this.state.seconds !== 900) {
+        this.textChoice("dialog");// AAAAAAAAAAA_AAAAAAAAH !
+        this.setState({ initialSeconds: this.state.seconds })
+      }
+      if (this.state.seconds === 900) {
+        this.textChoice("almostTimeUp");
+        this.setState({ closing: true, middle: false })
+      }
       this.updateDisplayedTime()
     }
   }
@@ -95,6 +184,15 @@ class App extends Component {
     });
     // It's set with updated time, now let's do it again
     setTimeout(this.takeOutOneSec, 1000)
+  }
+
+  textChoice(choice) {
+    let random = this.state[choice][Math.floor(Math.random() * this.state[choice].length)];
+    this.setState({ selected: random })
+    if (activeTab===false) {
+      this.gotNewNotification(random, "https://www.fightersgeneration.com/np7/char/hayato-rs-bust.png")
+    }
+    
   }
 
   // =============== O N C H A N G E ==================
@@ -114,6 +212,7 @@ class App extends Component {
   }
 
 
+
   render() {
     return (
       <AppBox>
@@ -124,7 +223,7 @@ class App extends Component {
 
           <Container>
             <BossBubble>
-              <BubbleText>{this.state.intro}</BubbleText>
+              <BubbleText>{this.state.selected}</BubbleText>
             </BossBubble>
             <BossImage
               src="https://www.fightersgeneration.com/np7/char/hayato-rs-bust.png" alt="Boss"
@@ -157,17 +256,12 @@ class App extends Component {
           </QuestionBlock>
 
           <div>
-            {
-              this.state.remainingMinutes > 9 ?
-                this.state.remainingMinutes : '0' + this.state.remainingMinutes
+            {this.state.remainingMinutes > 9 ?
+              this.state.remainingMinutes : '0' + this.state.remainingMinutes
             }:{
               this.state.remainingSeconds > 9 ?
-                this.state.remainingSeconds : '0' + this.state.remainingSeconds
-            }
+                this.state.remainingSeconds : '0' + this.state.remainingSeconds}
           </div>
-          <button onClick={this.gotNewNotification}>
-            Notify
-          </button>
 
         </Content>
         {/* <ToastContainer /> */}
